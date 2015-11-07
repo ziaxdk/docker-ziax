fd6e48a2c7f16112808e3242d71544b4a5a1be40
ZiaxDK <ziaxdk@gmail.com>
2015-11-07 09:31:00 +0000

    changed node port 8888->3000

--- containers/node/index.js
+++ containers/node/index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(8888, function () {
+var server = app.listen(process.env.APP_PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
